79ed1a381d63279fe138b4b08b285a804d4a40d8
Ivan B <ivar110kg@gmail.com>
2022-04-06 14:14:18 +0400

    update classes

--- com/github/ivan100kg/javablackbelt/lesson2/MainApp.java
+++ com/github/ivan100kg/javablackbelt/lesson2/MainApp.java
@@ -10,17 +10,25 @@ public class MainApp {
         System.out.println("Hello!");
         MyClass[] arr = {new MyClass(1), new MyClass(6), new MyClass(3)};
         System.out.println(Arrays.toString(arr));
-        Arrays.sort(arr);
+//        Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
 
         List<MyClass> list = new ArrayList<>(Arrays.asList(arr));
         list.add(new MyClass(1));
         System.out.println(list);
-        Collections.sort(list);
+//        Collections.sort(list);
         System.out.println(list);
 
         System.out.println(Integer.compare(2, 5));
         System.out.println("123".compareTo("234"));
+
+        MyClass m1 = new MyClass(8);
+        MyClass m2 = new MyClass(79);
+
+        IdComparator ic = new IdComparator();
+        System.out.println(ic.compare(m1, m2));
+        list.sort(ic);
+        Collections.sort(list, ic);
 
     }
 }
