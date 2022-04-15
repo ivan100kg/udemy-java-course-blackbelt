2e51a43c079564c4c3462cec13fb3d820f5f3889
Ivan B <ivar110kg@gmail.com>
2022-04-15 19:01:00 +0400

    start lesson5

---
+++ com/github/ivan100kg/javablackbelt/lesson5/MainApp.java
@@ -0,0 +1,14 @@
+package com.github.ivan100kg.javablackbelt.lesson5;
+
+public class MainApp {
+    static class StaticClass {  // static class
+    }
+
+    class InnerClass {          // inner class
+    }
+
+    void method() {
+        class LocalClass {      // local class
+        }
+    }
+}
