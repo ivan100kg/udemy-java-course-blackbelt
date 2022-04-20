05db7cb2d0697591c360cfaf9e1d491ad646d2c7
Ivan B <ivar110kg@gmail.com>
2022-04-20 19:38:18 +0400

    add fle Test3.java

---
+++ com/github/ivan100kg/javablackbelt/lesson6/Test3.java
@@ -0,0 +1,46 @@
+package com.github.ivan100kg.javablackbelt.lesson6;
+
+import java.util.ArrayList;
+import java.util.function.Supplier;
+import java.util.function.Consumer;
+
+public class Test3 {
+    public static ArrayList<Car> createThreeCars(Supplier<Car> carSupplier) {
+        ArrayList<Car> cars = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            cars.add(carSupplier.get());
+        }
+        return cars;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Car> arrayList = createThreeCars(() -> new Car("Nissan", "black", 225.0));
+        System.out.println(arrayList);
+        MySup(() -> "Hello");
+    }
+
+    public static void MySup(Supplier<String> stringSupplier) {
+        System.out.println(stringSupplier.get());
+    }
+}
+
+class Car {
+    String model;
+    String color;
+    Double engine;
+
+    public Car(String model, String color, Double engine) {
+        this.model = model;
+        this.color = color;
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", engine=" + engine +
+                '}';
+    }
+}
