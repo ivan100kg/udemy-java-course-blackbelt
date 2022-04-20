b5dc614872dcc86efaad1fe0ba5f17d24a516873
Ivan B <ivar110kg@gmail.com>
2022-04-20 19:53:53 +0400

    add consumer, supplier

--- com/github/ivan100kg/javablackbelt/lesson6/Test3.java
+++ com/github/ivan100kg/javablackbelt/lesson6/Test3.java
@@ -13,10 +13,16 @@ public class Test3 {
         return cars;
     }
 
+    public static void changeCar(Car car, Consumer<Car> carConsumer) {
+        carConsumer.accept(car);
+    }
+
     public static void main(String[] args) {
         ArrayList<Car> arrayList = createThreeCars(() -> new Car("Nissan", "black", 225.0));
         System.out.println(arrayList);
         MySup(() -> "Hello");
+        changeCar(arrayList.get(0), car -> car.engine/=2);
+        System.out.println(arrayList);
     }
 
     public static void MySup(Supplier<String> stringSupplier) {
