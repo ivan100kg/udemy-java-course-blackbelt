1b11faec15bad97e6d6e50cd88b29e406a1a2beb
Ivan B <ivar110kg@gmail.com>
2022-04-25 16:33:00 +0400

    add stream chaining

--- com/github/ivan100kg/javablackbelt/lesson7/Test5.java
+++ com/github/ivan100kg/javablackbelt/lesson7/Test5.java
@@ -5,7 +5,15 @@ import java.util.Arrays;
 public class Test5 {
     public static void main(String[] args) {
         int[] array = {3, 8, 1, 5, 9, 12, 65, 80, 0, 21, 3};
-        array = Arrays.stream(array).sorted().toArray();
-        System.out.println(Arrays.toString(array));
+//        array = Arrays.stream(array).sorted().toArray();
+//        System.out.println(Arrays.toString(array));
+        Arrays.stream(array).filter(e -> e % 2 != 0)
+                .map(e ->
+                {
+                    if (e % 3 == 0) e = -1;
+                    return e;
+                })
+                .sorted()
+                .forEach(el -> System.out.print(el + " "));
     }
 }
