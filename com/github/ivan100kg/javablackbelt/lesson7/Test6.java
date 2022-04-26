f0c23350b874afb9e02bd1674e3b539bb201d4a0
Ivan B <ivar110kg@gmail.com>
2022-04-26 09:57:36 +0400

    add Stream concat

--- com/github/ivan100kg/javablackbelt/lesson7/Test6.java
+++ com/github/ivan100kg/javablackbelt/lesson7/Test6.java
@@ -6,10 +6,16 @@ import java.util.stream.Stream;
 public class Test6 {
     public static void main(String[] args) {
         Stream<Integer> stream1 = Stream.of(1, 2, 3, 4, 5, 1, 2, 3);
-        stream1.filter(el -> {
-            System.out.println("!!!");
-            return el % 2 == 0;
-        }).collect(Collectors.toSet());
+//        stream1.filter(el -> {
+//            System.out.println("!!!");
+//            return el % 2 == 0;
+//        }).collect(Collectors.toSet());
+        Stream<Integer> stream2 = Stream.of(6, 6, 6);
+        Stream<Integer> stream3 = Stream.of(100, 200, 300);
+        Stream<Integer> stream4 = Stream.concat(stream2, stream3);
+//        System.out.println(stream4.collect(Collectors.toList()));
+        System.out.println(stream4.collect(Collectors.toSet()));
+
 
     }
 
