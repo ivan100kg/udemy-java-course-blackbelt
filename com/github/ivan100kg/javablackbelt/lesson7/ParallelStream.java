ddeb10d4d5a9f4585d30655b0ca48b21a819753d
Ivan B <ivar110kg@gmail.com>
2022-04-27 09:16:54 +0400

    add stream parallel

---
+++ com/github/ivan100kg/javablackbelt/lesson7/ParallelStream.java
@@ -0,0 +1,22 @@
+package com.github.ivan100kg.javablackbelt.lesson7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParallelStream {
+    public static void main(String[] args) {
+        List<Double> list = new ArrayList<>();
+        list.add(10.0);
+        list.add(5.0);
+        list.add(1.0);
+        list.add(0.25);
+
+        double sum1 = list.stream().mapToDouble(el -> el).sum();
+        double sum2 = list.stream().reduce(Double::sum).get();
+        double sumP = list.parallelStream().reduce((acc, el) -> acc + el).get();
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sumP);
+
+    }
+}
