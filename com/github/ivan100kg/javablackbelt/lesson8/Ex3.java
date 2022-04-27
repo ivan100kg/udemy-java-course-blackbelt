baddab175590898e269bc005c2c7bd8596e7abc2
Ivan B <ivar110kg@gmail.com>
2022-04-27 10:14:42 +0400

    add file Ex3.java

---
+++ com/github/ivan100kg/javablackbelt/lesson8/Ex3.java
@@ -0,0 +1,16 @@
+package com.github.ivan100kg.javablackbelt.lesson8;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                System.out.println("1: " + i);
+            }
+        }).start();
+        new Thread(() -> {
+            for (int i = 1000; i > 0; i--) {
+                System.out.println("2: " + i);
+            }
+        }).start();
+    }
+}
