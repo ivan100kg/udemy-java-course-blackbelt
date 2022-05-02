975307a68ac6ea6d8b787b539ee2a6812512b819
Ivan B <ivar110kg@gmail.com>
2022-05-02 12:16:26 +0400

    add file Ex10.java

---
+++ com/github/ivan100kg/javablackbelt/lesson8/Ex10.java
@@ -0,0 +1,32 @@
+package com.github.ivan100kg.javablackbelt.lesson8;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        MyRunnable2 myRunnable = new MyRunnable2();
+        Thread t1 = new Thread(myRunnable);
+        Thread t2 = new Thread(myRunnable);
+        Thread t3 = new Thread(myRunnable);
+        t1.start();
+        t2.start();
+        t3.start();
+
+    }
+}
+
+class Counter {
+    volatile static int count;
+}
+
+class MyRunnable2 implements Runnable {
+    synchronized public void inc() {
+        Counter.count++;
+        System.out.print(Counter.count + " ");
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            inc();
+        }
+    }
+}
--- com/github/ivan100kg/javablackbelt/lesson8/Ex7.java
+++ com/github/ivan100kg/javablackbelt/lesson8/Ex7.java
@@ -4,15 +4,15 @@ public class Ex7 {
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(new MyRunnable());
         t1.start();
-//        for (int i = 0; i < 10; i++) {
-//            try {
-//                Thread.sleep(200);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            System.out.println(Thread.currentThread().getName() + ": " + i);
-//        }
-        t1.join();
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + ": " + i);
+        }
+//        t1.join();
         System.out.println(Thread.currentThread().getName() + ": has finished");
 
     }
