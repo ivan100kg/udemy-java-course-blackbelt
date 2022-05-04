fac8fa80b7507a068268a6021c4533c4019d9bac
Ivan B <ivar110kg@gmail.com>
2022-05-04 17:47:48 +0400

    add file DeadLockEx.java

---
+++ com/github/ivan100kg/javablackbelt/lesson8/DeadLockEx.java
@@ -0,0 +1,37 @@
+package com.github.ivan100kg.javablackbelt.lesson8;
+
+public class DeadLockEx {
+    public static final Object lock1 = new Object();
+    public static final Object lock2 = new Object();
+
+    public static void main(String[] args) {
+        new Thread10().start();
+        new Thread20().start();
+    }
+}
+
+class Thread10 extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Thread10: trying to get the monitor of object lock1");
+        synchronized (DeadLockEx.lock1) {
+            System.out.println("Thread10: the monitor of object lock1 is gotten");
+            synchronized (DeadLockEx.lock2) {
+                System.out.println("Thread10: the monitors of objects lock1 and lock2 is gotten");
+            }
+        }
+    }
+}
+
+class Thread20 extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Thread20: trying to get the monitor of object lock2");
+        synchronized (DeadLockEx.lock2) {
+            System.out.println("Thread20: the monitor of object lock2 is gotten");
+            synchronized (DeadLockEx.lock1) {
+                System.out.println("Thread20: the monitors of objects lock1 and lock2 is gotten");
+            }
+        }
+    }
+}
