9ce1bade79c3613d1c805bbe91df735dfbd58a7e
Ivan B <ivar110kg@gmail.com>
2022-05-04 20:49:13 +0400

    test some features

--- com/github/ivan100kg/javablackbelt/lesson8/WaitNotifyApp.java
+++ com/github/ivan100kg/javablackbelt/lesson8/WaitNotifyApp.java
@@ -1,7 +1,19 @@
 package com.github.ivan100kg.javablackbelt.lesson8;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class WaitNotifyApp {
     volatile int count = 0;
     private final Object lock = new Object();
+
+    public static void main(String[] args) {
+        System.out.println(Boolean.hashCode(true));
+        System.out.println(Integer.compare(12, 12));
+
+        System.out.println(Stream.of(1, 2, 3, 4)
+                .map(String::valueOf)
+                .collect(Collectors.joining()));
+    }
 
 }
