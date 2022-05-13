14ae5c2e5f4e82ab31ae3660355d7b5ec4afc278
Ivan B <ivar110kg@gmail.com>
2022-05-13 18:01:50 +0400

    Fix SemaphoreEx.java

--- com/github/ivan100kg/javablackbelt/lesson8/SemaphoreEx.java
+++ com/github/ivan100kg/javablackbelt/lesson8/SemaphoreEx.java
@@ -4,18 +4,19 @@ import java.util.concurrent.Semaphore;
 
 public class SemaphoreEx {
     public static void main(String[] args) {
-        Semaphore smp = new Semaphore(4);
+        Semaphore smp = new Semaphore(1);
         for (int i = 0; i < 5; i++) {
+            final int w = i;
             new Thread(() -> {
                 try {
-                    System.out.println("Поток " + Thread.currentThread().getName() + " перед семафором");
+                    System.out.println("Поток " + w + " перед семафором");
                     smp.acquire();  // если счетчик не 0, захват и счетчик-=1
-                    System.out.println("Поток " + Thread.currentThread().getName() + " получил доступ к ресурсу");
+                    System.out.println("Поток " + w + " получил доступ к ресурсу");
                     Thread.sleep(3000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
-                    System.out.println("Поток " + Thread.currentThread().getName() + " освободил ресурс");
+                    System.out.println("Поток " + w + " освободил ресурс");
                     smp.release();  // увеличить счетчик на 1
                 }
             }).start();
