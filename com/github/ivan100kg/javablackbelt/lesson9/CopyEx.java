76fedbfb953bef4639afe3dbe1c9f6f1975f6476
Ivan B <ivar110kg@gmail.com>
2022-05-17 20:29:33 +0400

    Fix file CopyEx.java

--- com/github/ivan100kg/javablackbelt/lesson9/CopyEx.java
+++ com/github/ivan100kg/javablackbelt/lesson9/CopyEx.java
@@ -4,8 +4,8 @@ import java.io.*;
 
 public class CopyEx {
     public static void main(String[] args) {
-        String path1 = "C:\\Users\\ivan1\\IdeaProjects\\udemy-java-course-blackbelt\\com\\github\\ivan100kg\\javablackbelt\\lesson9\\rubai.txt";
-        String path2 = "C:\\Users\\ivan1\\IdeaProjects\\udemy-java-course-blackbelt\\com\\github\\ivan100kg\\javablackbelt\\lesson9\\rubai2.txt";
+        String path1 = "C:\\Users\\ivan1\\IdeaProjects\\udemy-java-course-blackbelt\\com\\github\\ivan100kg\\javablackbelt\\lesson9\\bb.jpg";
+        String path2 = "C:\\Users\\ivan1\\IdeaProjects\\udemy-java-course-blackbelt\\com\\github\\ivan100kg\\javablackbelt\\lesson9\\bb_copy.jpg";
         try (BufferedReader reader = new BufferedReader(new FileReader(path1));
              BufferedWriter writer = new BufferedWriter(new FileWriter(path2))) {
             String line;
