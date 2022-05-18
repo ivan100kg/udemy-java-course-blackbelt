b1fb6cace3de9c5ea559381a66b0c44a15d3ad5c
Ivan B <ivar110kg@gmail.com>
2022-05-18 20:32:07 +0400

    Update Serialization

--- com/github/ivan100kg/javablackbelt/lesson9/programmer1/Employee.java
+++ com/github/ivan100kg/javablackbelt/lesson9/programmer1/Employee.java
@@ -3,26 +3,27 @@ package com.github.ivan100kg.javablackbelt.lesson9.programmer1;
 import java.io.Serializable;
 
 public class Employee implements Serializable {
+    static final long serialVersionUID = 3;
     String name;
+    String familyName;
     String department;
-    int age;
-    double salary;
+    transient double salary;
     Car car;
 
-    public Employee(String name, String department, int age, double salary, Car car) {
+    public Employee(String name, String familyName, String department, double salary, Car car) {
         this.name = name;
+        this.familyName = familyName;
         this.department = department;
-        this.age = age;
         this.salary = salary;
         this.car = car;
     }
 
     @Override
     public String toString() {
-        return "Employees{" +
+        return "Employee{" +
                 "name='" + name + '\'' +
+                ", familyName='" + familyName + '\'' +
                 ", department='" + department + '\'' +
-                ", age=" + age +
                 ", salary=" + salary +
                 ", car=" + car +
                 '}';
--- com/github/ivan100kg/javablackbelt/lesson9/programmer1/SerializationEx2.java
+++ com/github/ivan100kg/javablackbelt/lesson9/programmer1/SerializationEx2.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class SerializationEx2 {
     public static void main(String[] args) {
-        Employee employee1 = new Employee("Ivan", "Backend", 39, 600000, new Car("Ford", "black"));
+        Employee employee1 = new Employee("Ivan", "Bond","Backend", 600000, new Car("Ford", "black"));
 
         try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("employees2.bin"))) {
             outputStream.writeObject(employee1);
