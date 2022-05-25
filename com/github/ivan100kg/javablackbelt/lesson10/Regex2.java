ae4802bdfdd614168e339ac21cc4fab905f3c03a
Ivan B <ivar110kg@gmail.com>
2022-05-25 21:09:10 +0400

    Add file Regex2.java

---
+++ com/github/ivan100kg/javablackbelt/lesson10/Regex2.java
@@ -0,0 +1,17 @@
+package com.github.ivan100kg.javablackbelt.lesson10;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex2 {
+    public static void main(String[] args) {
+        String s1 = "ABCD ABCE ABCDEFG";
+        Pattern p1 = Pattern.compile("ABCD");
+        Matcher matcher = p1.matcher(s1);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+
+    }
+}
