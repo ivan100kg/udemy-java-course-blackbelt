132e2411ef089600887c2bf79faf853f26d3a035
Ivan B <ivar110kg@gmail.com>
2022-05-26 15:53:35 +0400

    Test some regex

--- com/github/ivan100kg/javablackbelt/lesson10/Regex1.java
+++ com/github/ivan100kg/javablackbelt/lesson10/Regex1.java
@@ -1,4 +1,19 @@
 package com.github.ivan100kg.javablackbelt.lesson10;
 
 public class Regex1 {
+    public static void main(String[] args) {
+        String celebs = "        name         |    country    |  birthday  \n" +
+                "---------------------+---------------+------------\n" +
+                " Abbey_Lee           | Australia     | 1987-06-12\n" +
+                " Abbie(12)           | Russia        | 1993-04-14\n" +
+                " Abbie_Cornish       | Australia     | 1982-08-07\n" +
+                " Abella_Danger       | United States | 1995-11-19\n" +
+                " Adriana_Chechik     | United States | 1991-11-04\n" +
+                " Adrianne_Palicki    | United States | 1983-05-06\n" +
+                " Alaina_Huffman      | Canada        | 1980-04-17\n" +
+                " Aleksandra_Bortich  | Belarus       | 1994-09-24\n" +
+                " Alessandra_Ambrosio | Brazil        | 1981-04-11\n" +
+                " Alex_Grey           | United States | 1996-04-20";
+
+    }
 }
--- com/github/ivan100kg/javablackbelt/lesson10/Regex2.java
+++ com/github/ivan100kg/javablackbelt/lesson10/Regex2.java
@@ -6,10 +6,11 @@ import java.util.regex.Pattern;
 public class Regex2 {
     public static void main(String[] args) {
         String s1 = "ABCD ABCE ABCDEFG";
-        Pattern p1 = Pattern.compile("ABCD");
+        Pattern p1 = Pattern.compile(".");
         Matcher matcher = p1.matcher(s1);
 
         while (matcher.find()) {
+            System.out.print(matcher.start() + " ");
             System.out.println(matcher.group());
         }
 
