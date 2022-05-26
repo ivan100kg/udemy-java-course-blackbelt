d116fda476454c1ea68db6ab411aac47488def90
Ivan B <ivar110kg@gmail.com>
2022-05-26 16:11:19 +0400

    Test some regex(2)

--- com/github/ivan100kg/javablackbelt/lesson10/Regex1.java
+++ com/github/ivan100kg/javablackbelt/lesson10/Regex1.java
@@ -1,5 +1,8 @@
 package com.github.ivan100kg.javablackbelt.lesson10;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Regex1 {
     public static void main(String[] args) {
         String celebs = "        name         |    country    |  birthday  \n" +
@@ -15,5 +18,15 @@ public class Regex1 {
                 " Alessandra_Ambrosio | Brazil        | 1981-04-11\n" +
                 " Alex_Grey           | United States | 1996-04-20";
 
+//        String reg = "\\d{0,4}-\\d{1,2}-\\d{1,2}";
+//        String reg = "\\b\\w+\\b";
+        String reg = "\\d";
+        Pattern p1 = Pattern.compile(reg);
+        Matcher matcher = p1.matcher(celebs);
+
+        while (matcher.find()) {
+            System.out.print(matcher.start() + " ");
+            System.out.println(matcher.group());
+        }
     }
 }
