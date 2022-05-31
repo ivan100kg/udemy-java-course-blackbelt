e22601751d0593eb1e7794ed4953aa23d4017568
Ivan B <ivar110kg@gmail.com>
2022-05-31 21:32:39 +0400

    Add file Employee.java

--- com/github/ivan100kg/javablackbelt/lesson12/Test1.java
+++ com/github/ivan100kg/javablackbelt/lesson12/Test1.java
@@ -1,4 +1,32 @@
 package com.github.ivan100kg.javablackbelt.lesson12;
 
 public class Test1 {
+    public static void main(String[] args) {
+        Parent p = new Child("Ivan");
+        p.showInfo();
+    }
+}
+
+class Parent {
+    String name;
+
+    public Parent(String name) {
+        this.name = name;
+    }
+
+    @Deprecated
+    void showInfo() {
+        System.out.println("It's Parent class. Name = " + name);
+    }
+}
+
+class Child extends Parent{
+    public Child(String name) {
+        super(name);
+    }
+
+    @Override
+    void showInfo() {
+        System.out.println("It's Child class. Name = " + name);
+    }
 }
